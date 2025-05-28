74b9cd3b3da4a89907323b967e29e2afe317b41b
Rinatka777 <rinatsharifullin@gmail.com>
2025-05-28 14:22:00 +0300

    Cell method updated- draws a line from center to center of cells

--- graphics.py
+++ graphics.py
@@ -41,3 +41,52 @@ class Line:
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.has_top_wall:
+            top = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(top, "black")
+
+        if self.has_right_wall:
+            right = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(right, "black")
+
+        if self.has_bottom_wall:
+            bottom = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(bottom, "black")
+
+        if self.has_left_wall:
+            left = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(left, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        x1 = (self.__x1 + self.__x2) // 2
+        y1 = (self.__y1 + self.__y2) // 2
+        x2 = (to_cell.__x1 + to_cell.__x2) // 2
+        y2 = (to_cell.__y1 + to_cell.__y2) // 2
+
+        color = "gray" if undo else "red"
+        move_line = Line(Point(x1, y1), Point(x2, y2))
+        self.__win.draw_line(move_line, color)
+
+     
+
+
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from graphics import Window, Line, Point
-
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+# Cell 1: all walls (default)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell3 = Cell(win)
 
-    l = Line(Point(30, 400), Point(300, 300))
-    win.draw_line(l, "red")
-    
+    cell1.draw(50, 50, 100, 100)
+    cell2.draw(100, 50, 150, 100)
+    cell3.draw(150, 50, 200, 100)
+
+    cell1.draw_move(cell2)          # red move forward
+    cell2.draw_move(cell3)          # red move forward
+    cell3.draw_move(cell2, undo=False)  # gray backtrack
+    cell2.draw_move(cell1, undo=True)  # gray backtrack
+
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
